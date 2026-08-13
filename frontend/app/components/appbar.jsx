3459d0beb3a2d6a68fc1cf17c5104262874378f9
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2026-08-13 15:05:42 +0000

    UI overall|

--- frontend/app/components/appbar.jsx
+++ frontend/app/components/appbar.jsx
@@ -4,12 +4,17 @@ import { Separator } from "@heroui/react";
 import { Button } from "@heroui/react";
 import Sun from "@/app/icons/Sun";
 import Moon from "@/app/icons/Moon";
+import favicon from "@/public/favicon.svg";
+import Image from "next/image";
 
 const Appbar = () => {
   return (
     <div className="w-full sticky left-0">
       <div className=" flex flex-row justify-around items-center">
-        <h1 className="text-4xl font-bold">ChrisTalk</h1>
+        <div className="flex flex-row gap-2 items-center">
+          <Image src={favicon} width={50} alt="ChrisTalk favicon" />
+          <h1 className="text-4xl font-bold">ChrisTalk</h1>
+        </div>
         <div id="links">
           <ul className="flex gap-8 text-lg justify-between cursor-pointer">
             <li className="hover:underline">Home</li>
