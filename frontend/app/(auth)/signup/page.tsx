e65e22d31dfe728cce64e3da11121cca51994412
Mawunyo Hafoba <mawunyohafoba28@gmail.com>
2026-08-14 19:32:34 +0000

    SignIn and SignUp design

--- frontend/app/(auth)/signup/page.tsx
+++ frontend/app/(auth)/signup/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 import { Surface } from "@heroui/react";
 import { Form, Button } from "@heroui/react";
-import {
-  Description,
-  FieldError,
-  Input,
-  Label,
-  TextField,
-} from "@heroui/react";
+import { FieldError, Input, Label, TextField } from "@heroui/react";
 import { useState } from "react";
 import Image from "next/image";
 import community from "@/public/community.jpg";
@@ -16,32 +10,55 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   return (
-    <div className="flex flex-col h-full w-full justify-center items-center gap-0 ">
-      <Surface className="py-2 w-96 rounded-4xl">
-        <h1 className=" text-4xl text-center">SIGN UP</h1>
-      </Surface>
+    <div className="flex flex-col h-full w-full justify-center items-center gap-6 ">
+      <div className="relative z-10 -mb-6">
+        <Surface className="px-8 py-3 rounded-full" variant="secondary">
+          <Label className="text-2xl">SignIn</Label>
+        </Surface>
+      </div>
 
       <Surface
         className=" p-4 w-1/2 h-5/9  gap-6 flex flex-row rounded-4xl"
         variant="default"
       >
-        <Surface className="w-3/5 rounded-4xl relative" variant="tertiary">
+        <Surface
+          className="w-3/5 rounded-4xl relative overflow-hidden"
+          variant="tertiary"
+        >
           <Image
             src={community}
             alt="Community image"
-            className="inset-0 rounded-4xl"
+            fill
+            className="object-cover  rounded-4xl"
           />
         </Surface>
         <Surface
-          className="w-2/5  h-full  rounded-4xl flex flex-col justify-center items-center"
+          className="w-2/5  h-full gap-2  rounded-4xl flex flex-col justify-center items-center"
           variant="tertiary"
         >
-          <Form className="flex flex-col gap-4">
+          <Form
+            className="flex flex-col gap-4"
+            onSubmit={(e) => {
+              e.preventDefault();
+
+              const data = Object.fromEntries(new FormData(e.currentTarget));
+              console.log(data);
+            }}
+          >
             <TextField
               isRequired
               name="email"
               type="email"
               className=" gap-4 items-center "
+              onChange={(value) => {
+                setEmail(value);
+              }}
+              validate={(value) => {
+                if (!value) return "Email is required";
+                if (!/^\S+@\S+\.\S+$/.test(value))
+                  return "Enter a valid email address";
+                return null;
+              }}
             >
               <Label className="text-lg self-start">Email</Label>
               <Input
@@ -55,34 +72,47 @@ const SignUp = () => {
               isRequired
               name="password"
               type="password"
+              onChange={(value) => {
+                setPassword(value);
+              }}
               className=" gap-4 items-start"
+              validate={(value) => {
+                if (!value) return "Password is required";
+                if (value.length < 8) return "Must be at least 8 characters";
+                if (!/[A-Z]/.test(value))
+                  return "Must include an uppercase letter";
+                if (!/[0-9]/.test(value)) return "Must include a number";
+                return null;
+              }}
             >
-              <Label className="text-lg self-start">Email</Label>
+              <Label className="text-lg self-start">Password</Label>
               <Input type="password" className="text-lg "></Input>
               <FieldError />
             </TextField>
 
             <TextField
-              isRequired
-              name="password"
+              name="confirm"
               type="password"
               className=" gap-4 items-center"
+              validate={(value) => {
+                if (!value) return "Please confirm your password";
+                if (value !== password) return "Passwords do not match";
+                return null;
+              }}
             >
-              <Label className="text-lg self-start">Email</Label>
+              <Label className="text-lg self-start">Confirm Password</Label>
               <Input type="password" className="text-lg"></Input>
               <FieldError />
             </TextField>
 
-            <div className="flex gap-2">
-              <div className="flex flex-row gap-4 ">
-                <Button type="submit">
-                  {/* <Check /> */}
-                  Submit
-                </Button>
-                <Button type="reset" variant="secondary">
-                  Reset
-                </Button>
-              </div>
+            <div className="flex justify-between gap-2 w-full">
+              <Button type="submit">
+                {/* <Check /> */}
+                Submit
+              </Button>
+              <Button type="reset" variant="secondary">
+                Reset
+              </Button>
             </div>
           </Form>
         </Surface>
